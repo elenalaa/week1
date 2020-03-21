6a45f1ab5120da3a6ad6637be1fa7347ca2f7c3c
Elena Laakso <elena.laakso@metropolia.fi>
2020-03-21 21:53:36 +0200

    moro

---
+++ server-two/index.js
@@ -0,0 +1,30 @@
+'use strict';
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'pug');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('index');
+    //res.json ({
+        //message:'Hello World!',
+
+    //})
+});
+
+app.get('/catinfo', (req, res) => {
+    const cat = {
+        'name': 'Frank',
+        'age': 6,
+        'weight': 5,
+    };
+    res.json(cat);
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`);
+});
+
